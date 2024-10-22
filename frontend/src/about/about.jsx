a3fa4cc22d5190d9e763ebece03c2b27dead01dd
heberGustavo <heber.gbarbosa@hotmail.com>
2024-10-22 15:00:17 -0300

    Add data in page

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import PageHeader from "../template/pageHeader";
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name="Sobre" small="Nós"/>
+        <div>
+            <h2>Nossa História</h2>
+            <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl rhoncus viverra penatibus quam pellentesque</p>
+
+            <h2>Missão e Visão</h2>
+            <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl rhoncus viverra penatibus quam pellentesque</p>
+
+            <h2>Imprensa</h2>
+            <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl rhoncus viverra penatibus quam pellentesque</p>    
+        </div>
     </div>
 )
