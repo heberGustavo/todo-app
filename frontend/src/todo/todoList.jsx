eea3af5212d5ec2bdf74c37f2b7152385783245d
heberGustavo <heber.gbarbosa@hotmail.com>
2024-10-25 07:41:55 -0300

    Create action to render list and remove item from list

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import IconButton from "../template/iconButton";
 
-export default props => (
-    <div>
-        <h1>List</h1>
-    </div>
-)
+export default props => {
+    const renderRows = () => {
+        const list = props.list || [];
+        return list.map(item => (
+            <tr key={item._id}>
+                <td>{item.description}</td>
+                <td>
+                    <IconButton style="danger" icon="trash-o" onClick={() => props.handleRemove(item)} />
+                </td>
+            </tr>
+        ))
+    }
+
+    return (
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
