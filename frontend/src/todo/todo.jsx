31124b49324bdb325917b06d46a73998b95f7727
heberGustavo <heber.gbarbosa@hotmail.com>
2024-10-28 06:41:14 -0300

    Styling layout

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -19,6 +19,7 @@ export default class Todo extends Component {
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
         this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
         this.handleSearch = this.handleSearch.bind(this);
+        this.handleClear = this.handleClear.bind(this);
 
         this.refresh();
     }
@@ -59,6 +60,11 @@ export default class Todo extends Component {
         .then(resp => this.refresh(this.state.description))
     }
 
+    handleClear(){
+        this.setState({...this.state, description: "" })
+        this.refresh();
+    }
+
     render() {
         return (
             <div>
@@ -68,6 +74,7 @@ export default class Todo extends Component {
                     handleChange={this.handleChange}
                     handleAdd={this.handleAdd}
                     handleSearch={this.handleSearch}
+                    handleClear={this.handleClear}
                 />
                 <TodoList
                     list={this.state.list}
