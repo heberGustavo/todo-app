d307b5aa81f6d11127e375b2281b0781ec28af71
heberGustavo <heber.gbarbosa@hotmail.com>
2024-11-08 07:11:07 -0300

    migration to Redux

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,34 +1,62 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
 import Grid from "../template/grid";
 import IconButton from "../template/iconButton";
+import { add, changeDescription, clear, search } from "./todoActions";
 
-export default props => (
-    <div role="form" className="todoForm">
-        <Grid cols="12 9 10">
-            <input
-                id="description"
-                className="form-control"
-                placeholder="Adicione uma tarefa"
-                value={props.description}
-                onChange={props.handleChange}
-            />
-        </Grid>
-        <Grid cols="12 3 2">
-            <IconButton
-                style="primary"
-                icon="plus"
-                onClick={props.handleAdd}
-            />
-            <IconButton
-                style="info"
-                icon="search"
-                onClick={props.handleSearch}
-            />
-            <IconButton
-                style="default"
-                icon="close"
-                onClick={props.handleClear}
-            />
-        </Grid>
-    </div>
-)
+class TodoForm extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount() {
+        this.props.search();
+    }
+
+    render() {
+
+        const { add, search, clear, description } = this.props;
+
+        return (
+            <div role="form" className="todoForm">
+                <Grid cols="12 9 10">
+                    <input
+                        id="description"
+                        className="form-control"
+                        placeholder="Adicione uma tarefa"
+                        value={this.props.description}
+                        onChange={this.props.changeDescription}
+                    />
+                </Grid>
+                <Grid cols="12 3 2">
+                    <IconButton
+                        style="primary"
+                        icon="plus"
+                        onClick={() => add(description)}
+                    />
+                    <IconButton
+                        style="info"
+                        icon="search"
+                        onClick={search}
+                    />
+                    <IconButton
+                        style="default"
+                        icon="close"
+                        onClick={() => clear() }
+                    />
+                </Grid>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    description: state.todo.description
+})
+
+const mapDispatchToProps = dispatch =>
+    bindActionCreators({ add, changeDescription, search, clear }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
 import IconButton from "../template/iconButton";
 
-export default props => {
+import { markAsDone, markAsPending, remove } from "../todo/todoActions";
+
+const TodoList = props => {
     const renderRows = () => {
         const list = props.list || [];
         return list.map(item => (
             <tr key={item._id}>
                 <td className={item.done ? "markAsDone" : ""}>{item.description}</td>
                 <td>
-                    <IconButton style="success" icon="check" hide={item.done} onClick={() => props.handleMarkAsDone(item)} />
-                    <IconButton style="warning" icon="undo" hide={!item.done} onClick={() => props.handleMarkAsPending(item)} />
-                    <IconButton style="danger" icon="trash-o" onClick={() => props.handleRemove(item)} />
+                    <IconButton style="success" icon="check" hide={item.done} onClick={() => props.markAsDone(item)} />
+                    <IconButton style="warning" icon="undo" hide={!item.done} onClick={() => props.markAsPending(item)} />
+                    <IconButton style="danger" icon="trash-o" onClick={() => props.remove(item)} />
                 </td>
             </tr>
         ))
@@ -30,3 +35,12 @@ export default props => {
         </table>
     )
 }
+
+const mapStateToProps = state => ({
+    list: state.todo.list
+})
+
+const mapDispatchToProps = dispatch =>
+    bindActionCreators({ markAsDone, markAsPending, remove }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
